f74e78af58500847e8aa4982dae28efcf5fe5943
sreehithakambhampati <kambhampatisreehitha@gmail.com>
2025-06-02 18:35:05 +0530

    Added dotenv package

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose")
-const uri = "mongodb://127.0.0.1:27017/mern";
+const uri = process.env.MONGODB_URI;
 const connectDB = async() => {
     try {
         await mongoose.connect(uri);
         console.log("Connection successful to DB")
     } catch (error) {
-        console.log(error)
+        console.log("Connection failed")
+       
     }
 }
 module.exports = connectDB
