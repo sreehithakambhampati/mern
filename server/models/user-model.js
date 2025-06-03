a623c674add4bd44c7f462ce915f697b3a76c2b2
sreehithakambhampati <kambhampatisreehitha@gmail.com>
2025-06-03 16:58:40 +0530

    Securing pwd with bcrypt

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
 
 const userSchema = new mongoose.Schema({
     username:{
@@ -20,6 +21,26 @@ const userSchema = new mongoose.Schema({
     isAdmin:{
         type:Boolean,
         default:false
+    }
+})
+
+
+// This is a Mongoose middleware that runs before a user document is saved to the database. 
+userSchema.pre("save",async function(next){
+    const user = this;
+     // Check if the password has been modified
+    if(!user.isModified("password"))
+    {
+        // Skip hashing if password wasn't changed
+        next();
+    }
+    try {
+       const saltRound= 10;
+       const hash_password = await bcrypt.hash(user.password,saltRound);
+       user.password = hash_password
+    } catch (error) {
+        // Pass error to Mongoose's error handling
+        next(error)
     }
 })
 // A model is the JavaScript interface to interact with documents in a MongoDB collection using a defined structure.
