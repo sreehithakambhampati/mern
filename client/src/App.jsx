7f1c6f24ed9229e6e34f9c3c3a041e4d49d1b020
sreehithakambhampati <kambhampatisreehitha@gmail.com>
2025-06-08 15:50:58 +0530

    Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router";
-import { Home } from '../pages/Home';
-import { About } from '../pages/About';
-import { Contact } from '../pages/Contact';
-import { Login } from '../pages/Login';
-import { Register } from '../pages/Register';
-import { Service } from '../pages/Service';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { Service } from './pages/Service';
+import { Navbar } from './components/Navbar';
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
