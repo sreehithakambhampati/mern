a244a67e9bd66234106ec30157337e0c0478cbdf
sreehithakambhampati <kambhampatisreehitha@gmail.com>
2025-06-13 16:37:51 +0530

    Store Contact data in db

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,9 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../context/auth";
 
 
 export const About = () => {
+  const{user} = useAuth();
   return (
     <>
       <main>
@@ -9,7 +11,7 @@ export const About = () => {
           <div className="container grid grid-two-cols">
             <div className="hero-content">
               {/* <p>We care to cure your Health</p> */}
-
+              <p>Welcome{user?`${user.username} to our website`:"to our website"}</p>
               <h1>Why Choose Us? </h1>
               <p>
                 Expertise: Our team consists of experienced IT professionals who
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -30,13 +30,27 @@ export const Contact = () => {
     });
   };
 
-  // handle fomr getFormSubmissionInfo
-  const handleSubmit = (e) => {
+ 
+  const handleSubmit = async(e) => {
     e.preventDefault();
-
-    console.log(contact);
+    try {
+      const response = await fetch("http://localhost:5000/contact",{
+             method:"POST",
+             headers:{
+               "Content-Type":"application/json"
+             },
+             body:JSON.stringify(contact),
+      })
+      if(response.ok)
+      {
+        setContact({
+          message:""})
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
-
+  
 
 
   return (
@@ -101,17 +115,7 @@ export const Contact = () => {
           </section>
         </div>
 
-        <section className="mb-3">
-          {/* <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.2613173278896!2d73.91411937501422!3d18.562253982539413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c147b8b3a3bf%3A0x6f7fdcc8e4d6c77e!2sPhoenix%20Marketcity%20Pune!5e0!3m2!1sen!2sin!4v1697604225432!5m2!1sen!2sin"
-            width="100%"
-            height="450"
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe> */}
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3825.1983779508537!2d80.60587207438479!3d16.516079627322053!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a35efde680d5d07%3A0xc3ce43bbec7d20a7!2s10%2C%20Brahmin%20St%2C%20Durga%20Agraharam%2C%20Mallikarjunapeta%2C%20Vijayawada%2C%20Andhra%20Pradesh%20520001!5e0!3m2!1sen!2sin!4v1749466001966!5m2!1sen!2sin" width="100%" height="450"  allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-        </section>
+       
       </section>
     </>
   );
